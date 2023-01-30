9968697bf90ac33054c107a1a89ed6a0cfc943aa
krymov.dulat <krymov.dulat@gmail.com>
2023-01-30 15:09:40 +0600

    Added Bottom Sheet Dialog as dialog and Standard Bottom Sheet Dialog

---
+++ app/src/main/java/kz/kd/for_practice/BottomSheetDialog.kt
@@ -0,0 +1,16 @@
+package kz.kd.for_practice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetDialog: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.dialog_bottom_sheet, container, false)
+}
--- app/src/main/java/kz/kd/for_practice/MainActivity.kt
+++ app/src/main/java/kz/kd/for_practice/MainActivity.kt
@@ -2,10 +2,24 @@ package kz.kd.for_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnBST: Button = findViewById(R.id.btn_bsd)
+        btnBST.setOnClickListener {
+            BottomSheetDialog().show(supportFragmentManager, null)
+        }
+
+        val btnBSTStandard: Button = findViewById(R.id.btn_bsd_standard)
+        btnBSTStandard.setOnClickListener {
+            val coordinatorLayout: CoordinatorLayout = findViewById(R.id.coordinator)
+            coordinatorLayout.isVisible = !coordinatorLayout.isVisible
+        }
     }
 }
