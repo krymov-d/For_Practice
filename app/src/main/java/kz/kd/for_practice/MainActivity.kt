39cfdf577f3f7bb9c04710329d3dcbd93a3577d3
krymov.dulat <krymov.dulat@gmail.com>
2023-01-31 14:47:06 +0600

    Added bottom navigation bar

--- app/src/main/java/kz/kd/for_practice/MainActivity.kt
+++ app/src/main/java/kz/kd/for_practice/MainActivity.kt
@@ -2,10 +2,14 @@ package kz.kd.for_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.isVisible
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity(), BSDExpandable.BottomSheetListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +30,21 @@ class MainActivity : AppCompatActivity(), BSDExpandable.BottomSheetListener {
         val btnBSDExpandable: Button = findViewById(R.id.btn_bsd_expandable)
         btnBSDExpandable.setOnClickListener {
             BSDExpandable().show(supportFragmentManager, null)
+        }
+
+        val bNVMain: BottomNavigationView = findViewById(R.id.bnv)
+        bNVMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.first_page -> {
+                    Toast.makeText(this@MainActivity, "Call", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.second_page -> {
+                    Toast.makeText(this@MainActivity, "Camera", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
