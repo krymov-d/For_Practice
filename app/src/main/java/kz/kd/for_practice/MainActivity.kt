700f3ff0a7fc83dbf3b84c03898aae707a21e60a
krymov.dulat <krymov.dulat@gmail.com>
2023-02-13 15:33:55 +0600

    Created custom class CircledButton

--- app/src/main/java/kz/kd/for_practice/MainActivity.kt
+++ app/src/main/java/kz/kd/for_practice/MainActivity.kt
@@ -1,29 +1,81 @@
 package kz.kd.for_practice
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(
-            UnderlinedTextView(this).apply {
-                text = "General Kenobi"
-            }
-        )
+        setContentView(R.layout.activity_main)
     }
 }
 
 class UnderlinedTextView @JvmOverloads constructor(
     context: Context,
-    attr: AttributeSet? = null,
+    attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : AppCompatTextView(context, attr, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     init {
         paint.isUnderlineText = true
+    }
+}
+
+private const val COLOR_NONE = -1
+
+class CircledButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, defStyleAttr) {
+    private var radius = 0f
+    private var hasBorder = false
+    private var borderColor = COLOR_NONE
+    private var borderSize = 0f
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CircledButton,
+            0, 0
+        ).apply {
+            try {
+                radius = getDimensionPixelSize(R.styleable.CircledButton_radius, 0).toFloat()
+                hasBorder = getBoolean(R.styleable.CircledButton_hasBorder, false)
+                borderColor = getColor(R.styleable.CircledButton_borderColor, COLOR_NONE)
+                borderSize = getDimensionPixelSize(R.styleable.CircledButton_borderSize, 0).toFloat()
+            } finally {
+                recycle()
+            }
+        }
+
+        val shapeAppearanceModel: ShapeAppearanceModel = ShapeAppearanceModel().toBuilder().apply {
+            if (radius > 0) {
+                setAllCorners(
+                    CornerFamily.ROUNDED,
+                    radius
+                )
+            }
+        }.build()
+
+        val shapeDrawable = MaterialShapeDrawable(shapeAppearanceModel).apply {
+
+            if (hasBorder && borderColor != COLOR_NONE) {
+                strokeWidth = borderSize
+                strokeColor = ColorStateList.valueOf(borderColor)
+            }
+
+            fillColor = ColorStateList.valueOf(androidx.appcompat.R.attr.colorPrimary)
+        }
+
+        background = shapeDrawable
     }
 }
