c40544d8d29d3c04eb19c6454c2b90446bb6f44f
krymov.dulat <krymov.dulat@gmail.com>
2023-02-13 13:54:04 +0600

    Created custom class UnderlinedTextView

--- app/src/main/java/kz/kd/for_practice/MainActivity.kt
+++ app/src/main/java/kz/kd/for_practice/MainActivity.kt
@@ -1,11 +1,29 @@
 package kz.kd.for_practice
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+            UnderlinedTextView(this).apply {
+                text = "General Kenobi"
+            }
+        )
+    }
+}
+
+class UnderlinedTextView @JvmOverloads constructor(
+    context: Context,
+    attr: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attr, defStyleAttr) {
+
+    init {
+        paint.isUnderlineText = true
     }
 }
